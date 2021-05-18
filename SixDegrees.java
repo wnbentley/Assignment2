9ced5edbd29e03c8af96cb1d3f7c9b17097e7b69
wnbentley <78238986+wnbentley@users.noreply.github.com>
2021-05-17 21:04:47 -0700

    Update SixDegrees.java

--- SixDegrees.java
+++ SixDegrees.java
@@ -10,10 +10,10 @@ import org.json.simple.JSONArray;
 
 public class SixDegrees {
 
-    static Hashtable<String, LinkedList<String>> graph = new Hashtable<>();
+    static Hashtable<String, LinkedList<String>> graph = new Hashtable<>(); //create hashtable representation of graph
 
     public static void main(String[] args) {
-
+        //read in csv file and parse JSON
         try {
             Reader reader = new FileReader(args[0]);
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT);
@@ -28,17 +28,17 @@ public class SixDegrees {
                     JSONArray jsonArray = (JSONArray) object;
                     for (int i = 0; i < jsonArray.size(); i++) {
                         JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                        String actor = ((String) jsonObject.get("name")).toLowerCase();
-                        if (!graph.containsKey(actor)) {
+                        String actor = ((String) jsonObject.get("name")).toLowerCase(); //get as lower case
+                        if (!graph.containsKey(actor)) { //if graph doesn't have actor, put em in.
                             graph.put(actor, new LinkedList<>());
                         }
-                        for (Object item : jsonArray) {
+                        for (Object item : jsonArray) { // loop through and add previous actors connections and add them to graph if needed
                             JSONObject data = (JSONObject) item;
-                            String actor2 = ((String) data.get("name")).toLowerCase();
+                            String actor2 = ((String) data.get("name")).toLowerCase(); //get as lower case.
                             if (!graph.containsKey(actor2)) {
                                 graph.put(actor2, new LinkedList<>());
                             }
-                            addEdge(actor, actor2);
+                            addEdge(actor, actor2); //add edge between actors
                         }
                     }
                 }
@@ -48,10 +48,10 @@ public class SixDegrees {
         } catch (Exception e) {
             System.out.println("File " + args[0] + " is invalid or is in the wrong format.");
         }
-        controlLoop();
+        controlLoop();  //calls control loop for user input
 
     }
-
+    // adds the edges between actors in a bidirectional fashion.
     public static void addEdge(String actor1, String actor2) {
         LinkedList<String> l;
         if(!graph.containsKey(actor1)){
@@ -64,21 +64,21 @@ public class SixDegrees {
         l.add(actor2);
         graph.put(actor1,l);
     }
-
+    // Preforms a BFS since edge weights don't exist(as such, they're all equal), saves where we're going into path LL to print back out later when we find matches
     public static LinkedList<String> breadthFirstSearchWithPath(String actor1, String actor2) {
 
         LinkedList<String> path = new LinkedList<>();
         LinkedList<String> visited = new LinkedList<>();
 
-        for (String item : graph.keySet()) {
-            if (item.toLowerCase().equals(actor1)) {
-                path.add(item);
-                visited.add(item);
+        for (String name : graph.keySet()) { 
+            if (name.equals(actor1)) {
+                path.add(name);
+                visited.add(name);
             }
         }
 
         path.add(actor1);
-        while (!path.isEmpty()) {
+        while (!path.isEmpty()) {       
             String vertex = path.remove();
             visited.add(vertex);
             if (graph.get(vertex).contains(actor2)) {
@@ -104,7 +104,7 @@ public class SixDegrees {
         return null;
 
     }
-
+    //This is control loop that takes user input and make sures there is an actor that matches, or just keeps asking for input until we get two good actors
     static void controlLoop() {
         int loopControl1 = 0;
         int loopControl2 = 0;
@@ -114,7 +114,7 @@ public class SixDegrees {
 
         while (loopControl1 == 0) {
             System.out.print("Actor 1 name: ");
-            actor1 = scan.nextLine().toLowerCase();
+            actor1 = scan.nextLine().toLowerCase(); //input to lowercase
             if (!graph.containsKey(actor1)) {
                 System.out.println("No such actor.");
             } else {
@@ -124,7 +124,7 @@ public class SixDegrees {
         while (loopControl2 == 0) {
 
             System.out.print("Actor 2 name: ");
-            actor2 = scan.nextLine().toLowerCase();
+            actor2 = scan.nextLine().toLowerCase(); //input to lowercase
 
             if (!graph.containsKey(actor2)) {
                 System.out.println("No such actor");
@@ -132,14 +132,14 @@ public class SixDegrees {
                 loopControl2++;
             }
         }
-            try {findShortestPaths(actor1, actor2);}
+            try {findShortestPaths(actor1, actor2);}  //try, catch here because my computer ran out of memory a few times calling this funciton
             catch(Exception e) {
                 System.out.println("No such path or your computer is out of memory");
             }
         scan.close();
     }
 
-
+    // this function takes paths and then iterates through it printing shortest path to -> from
     static void findShortestPaths(String actor1, String actor2){
 
         LinkedList<String> find = breadthFirstSearchWithPath(actor1, actor2);
